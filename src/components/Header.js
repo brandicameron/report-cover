667c7dcd067a1c3579b744bfda047fb5d1de759d
Brandi Cameron <hello@brandicameron.com>
2022-08-03 16:41:30 -0500

    Changed logo link to external to prevent 404 page in production

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from '../images/chi-logo.svg';
 export default function Header() {
   return (
     <header>
-      <a href='/'>
+      <a href='https://brandicameron.github.io/report-cover/'>
         <img className='logo' src={Logo} alt='Cameron Home Inspection Logo' />
       </a>
     </header>
